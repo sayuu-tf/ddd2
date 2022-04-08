b01870493652724d34d0a08cad2f7489fdd90d9f
Yusuke Sakamoto <sakamoto@technoface.co.jp>
2022-04-08 15:45:15 +0900

    Test用Mock作成開始

---
+++ DDD2/DDD2.Domain/Entities/ItemEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD2.Domain.Entities
+{
+    public sealed class ItemEntity
+    {
+        string ItemName { get; }
+        int Price { get; }
+        int ShopId { get; }
+
+        public ItemEntity(string itemName, int price, int shopId)
+        {
+            ItemName = itemName;
+            Price = price;
+            ShopId = shopId;
+        }
+    }
+}
---
+++ DDD2/DDD2Test.Tests/ShopItemViewModelTest.cs
@@ -0,0 +1,17 @@
+﻿using DDD2.Domain.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace DDD2Test.Tests
+{
+    [TestClass]
+    public class ShopItemViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var itemMock = new Mock<IItemRepository>();
+        }
+    }
+}
