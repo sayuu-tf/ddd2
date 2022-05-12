e319b8d6e76d34c70f6736771ea64a6b0bf0d8ff
Yusuke Sakamoto <sakamoto@technoface.co.jp>
2022-05-12 14:37:39 +0900

    shop entity追加

---
+++ DDD2/DDD2.Domain/Entities/ShopEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD2.Domain.Entities
+{
+    public sealed class ShopEntity
+    {
+        public string ShopName { get; }
+        public string Location { get; }
+        public int ShopId { get; }
+
+        public ShopEntity(string shopName, string location, int shopId)
+        {
+            ShopName = shopName;
+            Location = location;
+            ShopId = shopId;
+        }
+    }
+}
